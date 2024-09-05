2587b5687c0840098fcc1bc0d65f6242137e8c7e
leliel <mail.leliel@proton.me>
2024-09-05 01:46:59 +0000

    modified font size again

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */ //
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "GohuFont uni11 Nerd Font Mono:monospace:size=11", };
-static const char dmenufont[]       = "GohuFont uni11 Nerd Font Mono:monospace:size=11";
+static const char *fonts[]          = { "GohuFont uni14 Nerd Font Mono:monospace:pixelsize=14:antialias=false:autohint=true", };
+static const char dmenufont[]       = "GohuFont uni14 Nerd Font Mono:monospace:pixelsize=14:antialias=false:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
